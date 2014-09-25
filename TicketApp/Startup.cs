5312d56f3ea8338cbb77ca08c76286b0c8c9a068
sujith <sujith.es@hotmail.com>
2014-09-25 18:37:53 +0530

    new solution project.

---
+++ TicketApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicketApp.Startup))]
+namespace TicketApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
